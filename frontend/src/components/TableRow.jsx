ae24ec66b21e7240b4e8dbf5e75fc75d4c1412a6
Brice Jenkins <brice.t.jenkins@gmail.com>
2025-07-31 00:29:59 -0700

    Changed edit icon to be available in all tables but incidents.

--- frontend/src/components/TableRow.jsx
+++ frontend/src/components/TableRow.jsx
@@ -2,25 +2,29 @@ import { MdCreate, MdDelete, MdOutlinePageview } from 'react-icons/md';
 
 const TableRow = ({ isIncidents, rowObject, onEdit, onView }) => {
     return (
-        <tr>{isIncidents &&
-            <td> 
-                <MdOutlinePageview
-                    className='view-icon'
-                    href='/'
-                    onClick = {e => {
-                        e.preventDefault();
-                        onView(rowObject)}}>
-                </MdOutlinePageview>
-            </td>}
-            <td>
-                <MdCreate
-                    className='edit-icon'
-                    href='/'
-                    onClick = {e => {
-                        e.preventDefault();
-                        onEdit(rowObject)}}>
-                </MdCreate>
-            </td>
+        <tr>
+            {isIncidents &&
+                <td> 
+                    <MdOutlinePageview
+                        className='view-icon'
+                        href='/'
+                        onClick = {e => {
+                            e.preventDefault();
+                            onView(rowObject)}}>
+                    </MdOutlinePageview>
+                </td>
+            }
+            {!isIncidents &&
+                <td>
+                    <MdCreate
+                        className='edit-icon'
+                        href='/'
+                        onClick = {e => {
+                            e.preventDefault();
+                            onEdit(rowObject)}}>
+                    </MdCreate>
+                </td>
+            }
             {Object.values(rowObject).map((value, index) => (
                 <td key={index}>{value}</td>
             ))}
